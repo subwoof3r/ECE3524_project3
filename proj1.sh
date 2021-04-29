2381ad9b64a5c7e132b0f71eb54339036f7fd4d5
Vewake Vallabhaneni <vewakev@vt.edu>
2021-04-28 23:38:54 -0400

    updated proj1.sh for use in proj3.sh

--- proj1.sh
+++ proj1.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-#Test for correct number of arguments
-if [ "$#" -ne 2 ] ; then
-  echo "Incorrect number of Arguments only 2 arguments should be provided"
-  exit 1
-fi
-
 #Test for file read permissions
-if ! [ -r $1 ] ; then
+if ! [ -r /home ] ; then
   echo "No read permission on file or directory"
   exit 1
 fi
 
 #generate tree
-find $1 | sed "s/[^-][^\/]*\//-/g" | sed ':a;N;$!ba;s/\n/<br>/g' > $2
+find $HOME | sed "s/[^-][^\/]*\//-/g" | sed ':a;N;$!ba;s/\n/<br>/g' > filetree.html
 
 exit 0
